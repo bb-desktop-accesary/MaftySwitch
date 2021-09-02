d54379522f2d245b50dbd00b1fd399473e5f088c
yuukiiwai <yuuki.20020217@gmail.com>
2021-09-02 12:16:03 +0900

    update : play wav file

--- Master.cs
+++ Master.cs
@@ -15,6 +15,9 @@ namespace MaftySwitch
     {
         public static Master master = null;
         private Bitmap gaw,lane,maf;
+        private System.Media.SoundPlayer gawW, laneW, mafW;
+        private bool SgawW, SlaneW, SmafW;
+        private int time = 0;
 
         public Master()
         {
@@ -25,10 +28,18 @@ namespace MaftySwitch
         private void Master_Load(object sender, EventArgs e)
         {
             this.TransparencyKey = this.BackColor;
+            this.SgawW = this.SlaneW = this.SmafW = false;
 
             GawBox.Location = new Point(GawBox.Location.X - 300, GawBox.Location.Y);
             MafBox.Location = new Point(MafBox.Location.X + 500, MafBox.Location.Y);
             LaneBox.Location = new Point(LaneBox.Location.X, LaneBox.Location.Y + 1080);
+
+            gawW = new System.Media.SoundPlayer(@".\src\gaw.wav");
+            gawW.Load();
+            mafW = new System.Media.SoundPlayer(@".\src\maf.wav");
+            mafW.Load();
+            laneW = new System.Media.SoundPlayer(@".\src\lane.wav");
+            laneW.Load();
 
         }
 
@@ -67,15 +78,47 @@ namespace MaftySwitch
 
         private void updateTimer_Tick(object sender, EventArgs e)
         {
-            if(GawBox.Location.X < 70)
+            if (GawBox.Location.X < 70)
             {
                 GawBox.Location = new Point(GawBox.Location.X + 10, GawBox.Location.Y);
-            }else if(MafBox.Location.X > 1350)
+                if (!SgawW)
+                {
+                    gawW.Play();
+                    SgawW = true;
+                }
+            } else if (time < 140)
+            {
+                time++;
+            }
+            else if (MafBox.Location.X > 1350)
             {
                 MafBox.Location = new Point(MafBox.Location.X - 15, MafBox.Location.Y);
-            }else if(LaneBox.Location.Y > 80)
+                if (!SmafW)
+                {
+                    mafW.Play();
+                    SmafW = true;
+                }
+            } else if (time < 200)
+            {
+                time++;
+            }
+            else if (LaneBox.Location.Y > 80)
             {
                 LaneBox.Location = new Point(LaneBox.Location.X, LaneBox.Location.Y - 40);
+                if (!SlaneW)
+                {
+                    laneW.Play();
+                    SlaneW = true;
+                }
+            }else if(time < 270)
+            {
+                time++;
+            }
+            else
+            {
+                MafBox.Dispose();
+                GawBox.Dispose();
+                LaneBox.Dispose();
             }
 
         }
